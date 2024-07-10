fcb7bc0cca91af50b1751e4447448081d9917676
AndyDev <andydev0101@gmail.com>
2024-07-10 03:00:57 +0300

    redesigned location, add map link

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -2,16 +2,13 @@ export function Location() {
   return (
     <section className="bg-[url('/location.jpg')] h-screen overflow-hidden  bg-cover bg-center flex items-center justify-center text-center">
       <div className="bg-white/90 w-2/5 p-16 flex flex-col items-center">
-        <p className="text-6xl pb-16 font-greatvibes">Локация</p>
-        <div className="w-10 h-1 bg-black mb-10"></div>
+        <p className="text-6xl  font-greatvibes">Локация</p>
+        <div className="w-10 h-1 bg-black my-16"></div>
         <p className="font-light text-xl mb-10">
-          Наша свадьба пройдет в *Тут будет кликабельная ссылка по названию
-          локации*
+          Наша свадьба пройдет в Ресторане Egoist расположенный на территории
+          курорта Новая Истра.
         </p>
-        <p className="font-light mb-10 text-xl">
-          она находится по адресу: *тут будет адресс*
-        </p>
-        <a href="">
+        <a href="https://yandex.ru/maps/?ll=36.798722%2C55.901347&mode=search&sll=36.792419%2C55.901374&text=55.901374%2C36.792419&z=16.68">
           <button className="text-white px-8 py-3 text-xl bg-black">
             ПЕРЕЙТИ К КАРТЕ
           </button>
--- src/components/SeeYouLater.jsx
+++ src/components/SeeYouLater.jsx
@@ -1,6 +1,9 @@
 export function SeeYouLater() {
   return (
-    <section className="flex flex-col gap-5 justify-center items-center bg-[url('/seeyou.jpg')]  h-screen bg-no-repeat bg-center bg-cover overflow-hidden ">
+    <section
+      className="flex flex-col gap-5 justify-center items-center bg-[url('/seeyou.jpg')]  h-screen bg-no-repeat bg-center bg-cover overflow-hidden text-shadow-custom-outline-white
+    "
+    >
       <p className="text-4xl font-greatvibes">До встречи!</p>
       <p className="text-4xl font-greatvibes mb-10">АЯ</p>
     </section>
